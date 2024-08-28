396ad0fc067c7138c9b157f5edd37b5690502a9f
boutoutben <boutout.ben@gmail.com>
2024-08-28 15:35:53 +0200

    heruko confif

---
+++ migrations/Version20240823092856.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240823092801 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("DROP INDEX id_habitat,id_race,id_last_rapport ON animal ");
+        $this->addSql('DROP INDEX id_animal ON rapport_veterinaire');
+    }
+
+}
