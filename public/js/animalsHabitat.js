02ccc4c7584f93f5397033fa2c3e5b1f21fedc75
boutoutben <boutout.ben@gmail.com>
2024-09-07 19:22:00 +0200

    make it better

--- public/js/animalsHabitat.js
+++ public/js/animalsHabitat.js
@@ -11,14 +11,7 @@ function presentation(id, nbAnimal, idAnimal)
 function hiddenPresentation(id, nbAnimal)
 {
         let presentation = document.getElementsByClassName("presentation_animaux");
-        let detail = document.getElementById(`rapportVet[${id}]`);
         presentation[(id)%nbAnimal].classList.remove("visible");
         presentation[(id)%nbAnimal].classList.add("hidden");
-        detail.classList.add("hidden")
 }
 
-function showDetail(id)
-{
-        let detail = document.getElementById(`rapportVet[${id}]`);
-        detail.classList.remove("hidden");
-}
