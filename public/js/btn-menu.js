e1e0e8d324544b264dec18911eff4335c96f4bd0
boutoutben <boutout.ben@gmail.com>
2024-09-13 22:00:42 +0200

    make it better

--- public/js/btn-menu.js
+++ public/js/btn-menu.js
@@ -1,9 +1,9 @@
 function showAndHideLink(){
     let link = document.getElementsByClassName("link");
-    if(link[0].style.display == "none"){
-        link[0].style.display = "flex";
+    if(link[0].classList.contains("visible") ){
+        link[0].classList.remove("visible");
     }
     else{
-        link[0].style.display = "none";
+        link[0].classList.add("visible");
     }
 }
